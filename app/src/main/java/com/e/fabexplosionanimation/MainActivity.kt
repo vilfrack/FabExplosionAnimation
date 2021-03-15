92fe27e2d08ab1a2a01f010c66ea1375c1a036f4
Juan villarroel <vilfrack21@gmail.com>
2021-03-15 11:05:40 -0300

    Se agrega animacion

--- app/src/main/java/com/e/fabexplosionanimation/MainActivity.kt
+++ app/src/main/java/com/e/fabexplosionanimation/MainActivity.kt
@@ -2,10 +2,34 @@ package com.e.fabexplosionanimation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.AnimationUtils
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
+import com.e.fabexplosionanimation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val animation = AnimationUtils.loadAnimation(this,R.anim.circle_explosion_animation).apply {
+            duration = 700
+            interpolator = AccelerateInterpolator()
+        }
+
+        binding.fab.setOnClickListener {
+            binding.fab.isVisible = false
+            binding.circle.isVisible = true
+            binding.circle.startAnimation(animation){
+                // display your fragment
+                binding.root.setBackgroundColor(ContextCompat.getColor(this, R.color.purple_500))
+                binding.circle.isVisible = false
+            }
+        }
     }
 }
